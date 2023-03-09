8e8128504c27ed24dd36db42ff88d3b14fc21fd0
Gabriel Kaam <gabriel@kaam.fr>
2023-03-09 14:32:26 +0100

    update.php

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chat-gpt-prompts-from-aiprm
+
+If you like these prompts, please consider visiting the project page and downloading their Chrome extension: https://www.aiprm.com/
+
+If am not affiliated with the AIPRM project.
--- update.php
+++ update.php
@@ -1,13 +1,51 @@
 <?php
 
 function sanitize($string) {
-	return preg_replace('/[^a-zA-Z0-9 ]/', '', $string);
+	$string = str_replace([
+			'&'
+		], [
+			'and'
+		], $string);
+	$string = preg_replace('/[^a-zA-Z0-9 \-,]/', '', $string);
+	$string = trim($string);
+
+	return $string;
+}
+
+function file_content_from_prompt($prompt) {
+	return <<<EOF
+AuthorName: {$prompt->AuthorName}
+AuthorURL: {$prompt->AuthorURL}
+
+Title: {$prompt->Title}
+Category: {$prompt->Category}
+Teaser: {$prompt->Teaser}
+
+Community: {$prompt->Community}
+CreationTime: {$prompt->CreationTime}
+Help: {$prompt->Help}
+ID: {$prompt->ID}
+OwnPrompt: {$prompt->OwnPrompt}
+PromptHint: {$prompt->PromptHint}
+PromptPackageID: {$prompt->PromptPackageID}
+PromptTypeNo": {$prompt->PromptTypeNo}
+RevisionTime: {$prompt->RevisionTime}
+Usages: {$prompt->Usages}
+Views: {$prompt->Views}
+Votes: {$prompt->Votes}
+
+Prompt:
+{$prompt->Prompt}
+
+EOF;
 }
 
 $prompts = json_decode(file_get_contents('https://api.aiprm.com/api2/Prompts?Community=&Limit=10&Offset=0&OwnerExternalID=user-Sym2oNwW2gUBywbi1gqkKPyB&OwnerExternalSystemNo=1&SortModeNo=2'));
 
 foreach ($prompts as $prompt) {
-	@mkdir('prompts/' . $prompt->Category);
-	echo $prompt->Title . " => '" . sanitize($prompt->Title) . "'\n";
-	echo "TEASER => {$prompt->Teaser}\n\n";
+	$path = 'prompts/' . $prompt->Category;
+	$name = strtolower(sanitize($prompt->Title));
+	@mkdir($path);
+
+	file_put_contents("$path/$name.txt", file_content_from_prompt($prompt));
 }

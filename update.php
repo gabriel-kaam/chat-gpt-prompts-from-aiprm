0d2d6d131425f40444d12d03ff3bb5329cb2b3ef
Gabriel Kaam <gabriel@kaam.fr>
2025-01-06 21:37:34 +0100

    Cache JSON files

--- update.php
+++ update.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-define('DATA_URL', 'https://api.aiprm.com/api9/Prompts?Topic=&Limit=10&Offset=0&OwnerOperatorERID=' . getenv('USER_ID') . '&OwnerSystemNo=1&SortModeNo=2&UserFootprint=&IncludeTeamPrompts=true');
+define('USER_ID', getenv('USER_ID'));
+define('DATA_URL', 'https://api.aiprm.com/api9/Prompts?Topic=&Limit=10&Offset=0&OwnerOperatorERID=' . USER_ID . '&OwnerSystemNo=1&SortModeNo=2&UserFootprint=&IncludeTeamPrompts=true');
 
 function sanitize($string) {
 	$string = str_replace([
@@ -38,6 +39,13 @@ Prompt:
 EOF;
 }
 
+function load_prompt($prompt) {
+	$raw = file_get_contents("https://api.aiprm.com/api9/Prompts/{$prompt->ID}?OperatorERID=" . USER_ID . "&SystemNo=1&Basic=true");
+	$data = json_decode($raw);
+
+	return $data->Prompt;
+}
+
 if(!$data = @file_get_contents(DATA_URL)) {
 	echo "[-] Could not get Data from API - ";
 	die(1);
@@ -45,11 +53,20 @@ if(!$data = @file_get_contents(DATA_URL)) {
 
 $prompts = json_decode($data);
 
+$i = 0;
+$max = count($prompts);
 foreach ($prompts as $prompt) {
 	$chunks = explode('/', trim($prompt->CanonicalURL, '/'));
 	$path = "prompts_v2/{$chunks[0]}/{$chunks[1]}";
 	$name = strtolower(sanitize($prompt->Title));
 	@mkdir($path, 0777, true);
+
+	echo "[".$i++."/{$max}]\tWorking on : $prompt->CanonicalURL\n";
+
+	if(empty($prompt->Prompt)) {
+		echo "\t[+] Loading ID: {$prompt->ID}\n";
+		$prompt->Prompt = load_prompt($prompt);
+	}
 
 	file_put_contents("$path/$name.txt", file_content_from_prompt($prompt));
 }
